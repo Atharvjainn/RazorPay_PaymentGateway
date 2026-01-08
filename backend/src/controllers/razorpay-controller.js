6843f42efbd0e9a96eeafa29e67815e2c8f4dcd8
Atharvjainn <jainatharv20@gmail.com>
2026-01-08 20:59:45 +0530

    completed backend part - createorder and verifypayment

--- backend/src/controllers/razorpay-controller.js
+++ backend/src/controllers/razorpay-controller.js
@@ -1,4 +1,6 @@
+const { RAZORPAY_KEY_SECRET } = require("../config/env");
 const razorpay = require("../config/razorpay.js");
+const crypto = require('crypto')
 
 const createOrder = async(req,res) => {
     try {
@@ -14,4 +16,32 @@ const createOrder = async(req,res) => {
         console.log("error in razorpay controller!");
         return res.status(500).json({message : "Internal Server Error"});
     }
+}
+
+
+const verifypayment = async(req,res) => {
+    try {
+        const { razorpay_order_id,razorpay_payment_id,razorpay_signature } = req.body;
+        const code = razorpay_order_id + "|" + razorpay_payment_id
+
+        const expectedSignature = crypto
+            .createHmac('sha256',RAZORPAY_KEY_SECRET)
+            .update(code)
+            .digest("hex")
+
+        if(expectedSignature == razorpay_signature){
+            return res.status(200).json({success : true})
+        }
+
+        return res.status(400).json({success : false})
+
+    } catch (error) {
+        console.log("error in razorpay controller!");
+        return res.status(500).json({message : "Internal Server Error"});
+    }
+}
+
+
+module.exports ={
+    createOrder,verifypayment
 }
